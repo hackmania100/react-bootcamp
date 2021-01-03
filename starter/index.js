f7005048850656987a31b3300121876e6af4a139
Yan Liu <32497439+zxyanliu@users.noreply.github.com>
2021-01-02 17:04:54 -0800

    update

--- starter/index.js
+++ starter/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // const element = React.createElement('div', null, 'React element!');
-const element = <div>React element with JSX!</div>;
+// const element = <div>React element with JSX!</div>;
 
-ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(element, document.getElementById('root'));
+
+// const element = React.createElement('div', null, 'React element!');
+
+const element = <div>React element with JSX!</div>
+ReactDOM.render(element,document.getElementById('root'));
